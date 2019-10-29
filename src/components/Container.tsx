b14f9fabfc1398e001ff8a70a3daa7159e640140
Deniz Bilgin <deniz@cara.care>
2019-10-29 19:18:06 +0100

    Container added

---
+++ src/components/Container.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  ViewStyle,
+  RegisteredStyle,
+} from 'react-native';
+import {SafeAreaView} from 'react-navigation';
+
+interface Props {
+  children: React.ReactNode;
+  bgColor?: string;
+  style?: ViewStyle | RegisteredStyle<ViewStyle>;
+  androidStatusBarMargin?: boolean;
+  iosStatusBarColor?: string;
+  iosBottomSafeArea?: boolean;
+}
+
+class Container extends React.Component<Props> {
+  static defaultProps = {
+    bgColor: '#fff',
+  };
+
+  render() {
+    const {
+      bgColor,
+      androidStatusBarMargin,
+      iosStatusBarColor,
+      style,
+      children,
+    } = this.props;
+    const computedStyles = [style, styles.container];
+
+    if (androidStatusBarMargin) {
+      computedStyles.push(styles.androidStatusBarMargin);
+    }
+
+    return (
+      <>
+        {iosStatusBarColor && (
+          <SafeAreaView
+            forceInset={{top: 'always', bottom: 'never'}}
+            style={{flex: 0, backgroundColor: iosStatusBarColor}}
+          />
+        )}
+        <SafeAreaView
+          forceInset={{
+            top: iosStatusBarColor ? 'never' : 'always',
+            bottom: this.props.iosBottomSafeArea ? 'always' : 'never',
+          }}
+          style={[styles.root, {backgroundColor: bgColor}]}>
+          <View style={computedStyles}>{children}</View>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  container: {
+    flexGrow: 1,
+  },
+  // Fixes the status bar overlap:
+  // eg: https://github.com/GeekyAnts/NativeBase/issues/899
+  androidStatusBarMargin: {
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight,
+      },
+    }),
+  },
+});
+
+export default Container;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Text from './components/Text';
 import Slider from './components/Slider';
 import BottomActionSheet from './components/BottomActionSheet';
+import Container from './components/Container';
 
-export {Text, Slider, BottomActionSheet};
+export {Text, Slider, BottomActionSheet, Container};
