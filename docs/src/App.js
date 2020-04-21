8c0ba64c417e2dba49d4b7fe5f6bfbf18ea84e29
Deniz Bilgin <deniz@cara.care>
2020-04-21 19:23:10 +0200

    Revert "converted to react project"
    This reverts commit 173a35bda3ac4d1128d5543c12f1b9d8426eb5d2.

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import StandardComponent from './StandardComponent';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <StandardComponent />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
