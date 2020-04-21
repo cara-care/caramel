47c98913f90ddca580c2b1a2f1573d87c0ef0262
Deniz Bilgin <deniz@cara.care>
2020-04-21 19:22:17 +0200

    Revert "css fixed"
    This reverts commit 9283982b17fdb51e4dc165a500f86ecfed4da523.

--- docs/src/StandardComponent.js
+++ docs/src/StandardComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './assets/css/standardcomponent.css';
 
 const StandardComponent = () => {
   return (
@@ -7,7 +6,7 @@ const StandardComponent = () => {
       <h2>Accordion</h2>
       <div className="imageTableContainer">
         <div>
-          <img style={{maxWidth: "300px", margin: "24px"}} src={require("./assets/Accordion.gif")} />
+          <img style={{maxWidth: "300px", margin: "24px"}} src="./assets/Accordion.gif" />
         </div>
         <div className="divTable">
           <div className="divTableBody">
@@ -72,22 +71,20 @@ list: {
       </div>
       <h4>Example</h4>
       <pre className="codeArea">
-        {
-`<Accordion
-  list={[
-      {
-        image: require('./caramel.png'),
-        name: 'Accordion1',
-        description: 'Lorem ipsum dolor sit amet.',
-      },
-      {
-        image: require('./caramel.png'),
-        name: 'Accordion2',
-        description: 'Lorem ipsum dolor sit amet.',
-      },
-    ]}
-  animate />`
-  }
+        {`&lt;Accordion
+      list={[
+          {
+            image: require('./caramel.png'),
+            name: 'Accordion1',
+            description: 'Lorem ipsum dolor sit amet.',
+          },
+          {
+            image: require('./caramel.png'),
+            name: 'Accordion2',
+            description: 'Lorem ipsum dolor sit amet.',
+          },
+        ]}
+      animate /&gt;`}
   </pre>
     </div>
   );
