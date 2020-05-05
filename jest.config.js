27f9c00e7204c4a49b2414ad3d89bd147200b146
Jan Czizikow <jan@gohidoc.com>
2020-05-05 15:30:48 +0200

    setup jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/node_modules/react-native/Libraries/react-native/',
+  ],
+  transformIgnorePatterns: ['node_modules/@react-navigation/.*'],
+};
