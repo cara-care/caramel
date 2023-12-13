710fcfb3cb542bf390f372a329ee3f3f6869cf74
Mo Shaban <mohamed.shaban@cara.care>
2023-12-13 11:16:13 +0100

    Add children prop to Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 
-interface Props {}
+interface Props {
+  children?: React.ReactNode;
+}
 
 class Container extends Component<Props> {
   render() {
